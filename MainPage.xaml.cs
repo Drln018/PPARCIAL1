a72a14e2ac3c91ff8aceb7ec8c8028c7b61cf37f
Drln018 <79181952+Drln018@users.noreply.github.com>
2021-03-05 15:38:40 -0600

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HOYSI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Anum_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async void Afecha_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+
+        void Exit_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
